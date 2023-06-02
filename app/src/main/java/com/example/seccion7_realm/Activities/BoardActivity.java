8041e7fae58f2a82cf79750bf5f1e9cd33c5b52d
jreyes1990 <jarl1990mt@gmail.com>
2023-06-01 21:37:11 -0600

    Video 78. Realm - FAB en ListView.

--- app/src/main/java/com/example/seccion7_realm/Activities/BoardActivity.java
+++ app/src/main/java/com/example/seccion7_realm/Activities/BoardActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.seccion7_realm.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BoardActivity extends AppCompatActivity {
+  private FloatingActionButton fab;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_board);
+
+    fab = (FloatingActionButton) findViewById(R.id.fabAddBoard);
   }
 }
