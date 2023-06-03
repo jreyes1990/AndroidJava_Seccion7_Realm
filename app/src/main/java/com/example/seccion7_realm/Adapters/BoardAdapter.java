efdf60a996b8bce5972b1e23adba50652416b78e
jreyes1990 <jarl1990mt@gmail.com>
2023-06-03 17:42:17 -0600

    Video 82. Realm - Creando Adaptador.

---
+++ app/src/main/java/com/example/seccion7_realm/Adapters/BoardAdapter.java
@@ -0,0 +1,74 @@
+package com.example.seccion7_realm.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.seccion7_realm.Models.Board;
+import com.example.seccion7_realm.R;
+
+import java.util.List;
+
+public class BoardAdapter extends BaseAdapter {
+  private Context context;
+  private List<Board> list;
+  private int layout;
+
+  public BoardAdapter(Context context, List<Board> list, int layout) {
+    this.context = context;
+    this.list = list;
+    this.layout = layout;
+  }
+
+  @Override
+  public int getCount() {
+    return list.size();
+  }
+
+  @Override
+  public Board getItem(int position) {
+    return list.get(position);
+  }
+
+  @Override
+  public long getItemId(int id) {
+    return id;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup viewGroup) {
+    ViewHolder vh;
+
+    if (convertView == null){
+      convertView = LayoutInflater.from(context).inflate(layout, null);
+      vh = new ViewHolder();
+      vh.title = (TextView) convertView.findViewById(R.id.textViewBoardTitle);
+      vh.notes = (TextView) convertView.findViewById(R.id.textViewBoardNotes);
+      vh.createdAt = (TextView) convertView.findViewById(R.id.textViewBoardDate);
+      convertView.setTag(vh);
+    }else {
+      vh = (ViewHolder) convertView.getTag();
+    }
+
+    Board board = list.get(position);
+
+    vh.title.setText(board.getTitle());
+
+    int numberOfNotes = board.getNotes().size();
+    String textForNotes = (numberOfNotes ==1) ? numberOfNotes+" Note" : numberOfNotes+" Notes";
+    vh.notes.setText(textForNotes);
+
+    vh.createdAt.setText(board.getCreatedAt().toString());
+
+    return null;
+  }
+
+  public class ViewHolder{
+    TextView title;
+    TextView notes;
+    TextView createdAt;
+  }
+}
