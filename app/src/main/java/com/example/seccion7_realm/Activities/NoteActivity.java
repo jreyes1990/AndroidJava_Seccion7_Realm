274f029a20786ee6d1a966f3b306012e738b400e
jreyes1990 <jarl1990mt@gmail.com>
2023-06-04 14:25:09 -0600

    Video 88. Realm - Editar y Borrar notas.

--- app/src/main/java/com/example/seccion7_realm/Activities/NoteActivity.java
+++ app/src/main/java/com/example/seccion7_realm/Activities/NoteActivity.java
@@ -1,12 +1,17 @@
 package com.example.seccion7_realm.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -60,6 +65,8 @@ public class NoteActivity extends AppCompatActivity implements RealmChangeListen
         showAlertForCreatingNote("Add New Note", "Type a note for "+board.getTitle()+".");
       }
     });
+
+    registerForContextMenu(listView);
   }
 
   //** CRUD Actions **//
@@ -80,6 +87,25 @@ public class NoteActivity extends AppCompatActivity implements RealmChangeListen
       }
     });
      */
+  }
+
+  private void editNote(String newNoteDescription, Note note){
+    realm.beginTransaction();
+    note.setDescripcion(newNoteDescription);
+    realm.copyToRealmOrUpdate(note);
+    realm.commitTransaction();
+  }
+
+  private void deleteNote(Note note){
+    realm.beginTransaction();
+    note.deleteFromRealm();
+    realm.commitTransaction();
+  }
+
+  private void deleteAll(){
+    realm.beginTransaction();
+    board.getNotes().deleteAllFromRealm();
+    realm.commitTransaction();
   }
 
   //** Dialogs **//
@@ -109,6 +135,76 @@ public class NoteActivity extends AppCompatActivity implements RealmChangeListen
 
     AlertDialog dialog = builder.create();
     dialog.show();
+  }
+
+  private void showAlertForEditingNote(String title, String message, final Note note){
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+    if (title != null) builder.setTitle(title);
+    if (message != null) builder.setMessage(message);
+
+    View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_note, null);
+    builder.setView(viewInflated);
+
+    final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewNote);
+    input.setText(note.getDescripcion());
+
+    builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialogInterface, int i) {
+        String noteDescription = input.getText().toString().trim();
+
+        if (noteDescription.length() == 0){
+          Toast.makeText(getApplicationContext(), "The text for the note is required to be edited", Toast.LENGTH_LONG).show();
+        } else if (noteDescription.equals(note.getDescripcion())){
+          Toast.makeText(getApplicationContext(), "The note is the same than it was before", Toast.LENGTH_LONG).show();
+        } else {
+          editNote(noteDescription, note);
+        }
+      }
+    });
+
+    AlertDialog dialog = builder.create();
+    dialog.show();
+  }
+
+  /* Events */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_note_activity, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()){
+      case R.id.deleteAllNotes:
+        deleteAll();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  @Override
+  public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    getMenuInflater().inflate(R.menu.context_menu_note_activity, menu);
+  }
+
+  @Override
+  public boolean onContextItemSelected(@NonNull MenuItem item) {
+    AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+    switch (item.getItemId()){
+      case R.id.delete_note:
+        deleteNote(notes.get(info.position));
+        return true;
+      case R.id.edit_note:
+        showAlertForEditingNote("Edit Note", "Change the name of the note", notes.get(info.position));
+        return true;
+      default:
+        return super.onContextItemSelected(item);
+    }
   }
 
   @Override
